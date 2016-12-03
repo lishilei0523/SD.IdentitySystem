4b860857df5a45560fc4eed3e1d02bd33661990f
Lee <lishilei0523@163.com>
2016-12-03 15:21:54 +0800

    整理代码，更新nuget包

--- tests/SD.IdentitySystem.StubServer/Stubs/StubAuthenticationContract.cs
+++ tests/SD.IdentitySystem.StubServer/Stubs/StubAuthenticationContract.cs
@@ -1,6 +1,6 @@
 ﻿using System;
 using SD.IdentitySystem.IAppService.Interfaces;
-using ShSoft.ValueObjects.Structs;
+using ShSoft.Infrastructure.Constants;
 
 namespace SD.IdentitySystem.StubServer.Stubs
 {
@@ -9,10 +9,10 @@ namespace SD.IdentitySystem.StubServer.Stubs
         /// <summary>
         /// 登录
         /// </summary>
-        /// <param name="loginId">登录名</param><param name="password">密码</param><param name="ip">IP地址</param>
-        /// <returns>
-        /// 公钥
-        /// </returns>
+        /// <param name="loginId">登录名</param>
+        /// <param name="password">密码
+        /// </param><param name="ip">IP地址</param>
+        /// <returns>登录信息</returns>
         public LoginInfo Login(string loginId, string password, string ip)
         {
             return null;
