ecb23102a64e72fe7e242fc8d4d62b8d53c03ad5
Lee <lishilei0523@163.com>
2016-12-14 20:42:30 +0800

    整理代码

---
+++ tools/SD.IdentitySystem.WCFAuthentication.MVC/AuthenticationMessageInspector.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.ServiceModel;
+using System.ServiceModel.Channels;
+using System.ServiceModel.Dispatcher;
+using System.Web;
+using ShSoft.Infrastructure.Constants;
+using ShSoft.Infrastructure.MVC.Constants;
+using ShSoft.ValueObjects;
+
+namespace SD.IdentitySystem.WCFAuthentication.MVC
+{
+    /// <summary>
+    /// WCF/MVC客户端身份认证消息拦截器
+    /// </summary>
+    internal class AuthenticationMessageInspector : IClientMessageInspector
+    {
+        /// <summary>
+        /// 请求发送前事件
+        /// </summary>
+        /// <param name="request">请求消息</param>
+        /// <param name="channel">信道</param>
+        /// <returns></returns>
+        public object BeforeSendRequest(ref Message request, IClientChannel channel)
+        {
+            //MVC客户端获取公钥处理
+            object loginInfo = HttpContext.Current.Session[SessionConstants.CurrentUserKey];
+
+            #region # 非空验证
+
+            if (loginInfo == null)
+            {
+                throw new ApplicationException("未登录，请重新登录！");
+            }
+
+            #endregion
+
+            Guid publishKey = ((LoginInfo)loginInfo).PublicKey;
+
+            //添加消息头
+            MessageHeader header = MessageHeader.CreateHeader(Constants.WcfAuthHeaderName, Constants.WcfAuthHeaderNamespace, publishKey);
+            request.Headers.Add(header);
+
+            return null;
+        }
+
+        /// <summary>
+        /// 请求响应后事件
+        /// </summary>
+        /// <param name="reply">响应消息</param>
+        /// <param name="correlationState">相关状态</param>
+        public void AfterReceiveReply(ref Message reply, object correlationState) { }
+    }
+}
---
+++ tools/SD.IdentitySystem.WCFAuthentication.WCF/AuthenticationMessageInspector.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Linq;
+using System.ServiceModel;
+using System.ServiceModel.Channels;
+using System.ServiceModel.Dispatcher;
+using SD.IdentitySystem.IAppService.Interfaces;
+using SD.IOC.Core.Mediator;
+using ShSoft.ValueObjects;
+
+namespace SD.IdentitySystem.WCFAuthentication.WCF
+{
+    /// <summary>
+    /// WCF客户端/服务端身份认证消息拦截器
+    /// </summary>
+    internal class AuthenticationMessageInspector : IDispatchMessageInspector, IClientMessageInspector
+    {
+        #region # Implements of IDispatchMessageInspector
+
+        /// <summary>
+        /// 接收请求后事件
+        /// </summary>
+        /// <param name="request">请求消息</param>
+        /// <param name="channel">信道</param>
+        /// <param name="instanceContext">WCF实例上下文</param>
+        /// <returns></returns>
+        public object AfterReceiveRequest(ref Message request, IClientChannel channel, InstanceContext instanceContext)
+        {
+            IAuthenticationContract authenticationContract = ResolveMediator.Resolve<IAuthenticationContract>();
+
+            //获取消息头
+            MessageHeaders headers = OperationContext.Current.IncomingMessageHeaders;
+
+            #region # 验证消息头
+
+            if (!headers.Any(x => x.Name == Constants.WcfAuthHeaderName && x.Namespace == Constants.WcfAuthHeaderNamespace))
+            {
+                throw new NullReferenceException("身份认证消息头不存在，请检查程序！");
+            }
+
+            #endregion
+
+            //读取消息头中的公钥
+            Guid publishKey = headers.GetHeader<Guid>(Constants.WcfAuthHeaderName, Constants.WcfAuthHeaderNamespace);
+
+            if (OperationContext.Current.EndpointDispatcher.ContractName != typeof(IAuthenticationContract).Name)
+            {
+                //认证
+                authenticationContract.Authenticate(publishKey);
+            }
+
+            return null;
+        }
+
+        /// <summary>
+        /// 响应请求前事件
+        /// </summary>
+        /// <param name="reply">响应消息</param>
+        /// <param name="correlationState">相关状态</param>
+        public void BeforeSendReply(ref Message reply, object correlationState) { }
+
+        #endregion
+
+        #region # Implements of IClientMessageInspector
+
+        /// <summary>
+        /// 请求发送前事件
+        /// </summary>
+        /// <param name="request">请求消息</param>
+        /// <param name="channel">信道</param>
+        /// <returns></returns>
+        public object BeforeSendRequest(ref Message request, IClientChannel channel)
+        {
+            //WCF客户端获取公钥处理
+            MessageHeaders headers = OperationContext.Current.IncomingMessageHeaders;
+
+            #region # 验证消息头
+
+            if (!headers.Any(x => x.Name == Constants.WcfAuthHeaderName && x.Namespace == Constants.WcfAuthHeaderNamespace))
+            {
+                throw new NullReferenceException("身份认证消息头不存在，请检查程序！");
+            }
+
+            #endregion
+
+            //读取消息头中的公钥
+            Guid publishKey = headers.GetHeader<Guid>(Constants.WcfAuthHeaderName, Constants.WcfAuthHeaderNamespace);
+
+            //添加消息头
+            MessageHeader header = MessageHeader.CreateHeader(Constants.WcfAuthHeaderName, Constants.WcfAuthHeaderNamespace, publishKey);
+            request.Headers.Add(header);
+
+            return null;
+        }
+
+        /// <summary>
+        /// 请求响应后事件
+        /// </summary>
+        /// <param name="reply">响应消息</param>
+        /// <param name="correlationState">相关状态</param>
+        public void AfterReceiveReply(ref Message reply, object correlationState) { }
+
+        #endregion
+    }
+}
