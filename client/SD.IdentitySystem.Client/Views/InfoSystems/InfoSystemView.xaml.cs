d82f89aee44107a6c95bc3e1308324ea095c72d6
Lee <lishilei0523@163.com>
2017-11-20 22:22:29 +0800

    信息系统管理列表页面完成

---
+++ client/SD.IdentitySystem.Client/Views/InfoSystems/InfoSystemView.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+
+namespace SD.IdentitySystem.Client.Views.InfoSystems
+{
+    /// <summary>
+    /// InfoSystemView.xaml 的交互逻辑
+    /// </summary>
+    public partial class InfoSystemView : UserControl
+    {
+        public InfoSystemView()
+        {
+            InitializeComponent();
+        }
+    }
+}
