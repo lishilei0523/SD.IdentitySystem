8078da729028ea2c64ab7722b90567e525fd7fef
Lee <lishilei0523@163.com>
2018-02-05 09:51:25 +0800

    更新nuget包

--- tools/SD.IdentitySystem.InitializationTool/MainWindow.cs
+++ tools/SD.IdentitySystem.InitializationTool/MainWindow.cs
@@ -9,6 +9,7 @@ using System.Linq;
 using System.Reflection;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using SD.Infrastructure.MemberShip;
 
 namespace SD.IdentitySystem.InitializationTool
 {
