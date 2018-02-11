763a5367a5c8c4a87b39da79c19851e92540e385
Lee <lishilei0523@163.com>
2018-02-11 08:55:25 +0800

    整理代码

--- service/SD.IdentitySystem.AppService/Global.asax
+++
@@ -1,1 +0,0 @@
-﻿<%@ Application Codebehind="Global.asax.cs" Inherits="SD.IdentitySystem.AppService.Global" Language="C#" %>
--- service/SD.IdentitySystem.Repository/Base/DbSession.cs
+++ service/SD.IdentitySystem.Repository/Base/DbSession.cs
@@ -1,8 +1,4 @@
-﻿using System.Configuration;
-using System.Data.Entity;
-using SD.Infrastructure.Constants;
-using SD.Infrastructure.Repository.EntityFramework.Base;
-using Configuration = SD.IdentitySystem.Repository.Migrations.Configuration;
+﻿using SD.Infrastructure.Repository.EntityFramework.Base;
 
 namespace SD.IdentitySystem.Repository.Base
 {
@@ -11,19 +7,6 @@ namespace SD.IdentitySystem.Repository.Base
     /// </summary>
     internal class DbSession : BaseDbSession
     {
-        /// <summary>
-        /// 静态构造器
-        /// </summary>
-        static DbSession()
-        {
-            //读取配置文件，是否开启自动数据迁移
-            bool enableMiagration = bool.Parse(ConfigurationManager.AppSettings[CommonConstants.AutoMigrationAppSettingKey]);
 
-            if (enableMiagration)
-            {
-                //数据迁移
-                Database.SetInitializer(new MigrateDatabaseToLatestVersion<DbSession, Configuration>());
-            }
-        }
     }
 }
