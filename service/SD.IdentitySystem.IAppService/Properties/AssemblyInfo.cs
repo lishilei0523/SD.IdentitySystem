b62636f2efa8e0a4cb519dc072d92fecc73e93fe
Lee <lishilei0523@163.com>
2018-05-09 23:00:30 +0800

    变更为引用拷贝

--- service/SD.IdentitySystem.IAppService/Properties/AssemblyInfo.cs
+++ service/SD.IdentitySystem.IAppService/Properties/AssemblyInfo.cs
@@ -9,7 +9,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("SD")]
 [assembly: AssemblyProduct("SD.IdentitySystem.IAppService")]
-[assembly: AssemblyCopyright("Copyright © SD 2016")]
+[assembly: AssemblyCopyright("Copyright © SD 2018")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
