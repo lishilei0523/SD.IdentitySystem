998de539dd97ca8cfbd9d776fd2408e333a8adaa
Lee <lishilei0523@163.com>
2021-01-29 16:49:54 +0800

    补充用例；

---
+++ Tests/SD.IdentitySystem.WebApi.Tests/Models/OrderDetail.cs
@@ -0,0 +1,9 @@
+﻿namespace SD.IdentitySystem.WebApi.Tests.Models
+{
+    public class OrderDetail
+    {
+
+        public string Number { get; set; }
+        public string Description { get; set; }
+    }
+}
