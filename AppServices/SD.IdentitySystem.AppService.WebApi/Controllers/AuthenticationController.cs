dbb5ff99141bad0bf7aef5fef011459d61e22186
Lee <lishilei0523@163.com>
2021-02-22 14:12:33 +0800

    增加WebApi代理；

---
+++ AppServices/SD.IdentitySystem.AppService.WebApi/Controllers/AuthenticationController.cs
@@ -0,0 +1,51 @@
+﻿using SD.IdentitySystem.IAppService.Interfaces;
+using SD.Infrastructure.MemberShip;
+using SD.Toolkits.WebApi.Extensions;
+using System.Web.Http;
+
+namespace SD.IdentitySystem.AppService.WebApi.Controllers
+{
+    /// <summary>
+    /// 身份认证WebApi接口
+    /// </summary>
+    public class AuthenticationController : ApiController
+    {
+        #region # 字段及构造器
+
+        /// <summary>
+        /// 身份认证服务契约接口
+        /// </summary>
+        private readonly IAuthenticationContract _authenticationContract;
+
+
+        /// <summary>
+        /// 依赖注入构造器
+        /// </summary>
+        public AuthenticationController(IAuthenticationContract authenticationContract)
+        {
+            this._authenticationContract = authenticationContract;
+        }
+
+        #endregion
+
+
+        //命令部分
+
+        #region # 登录 —— LoginInfo Login(string loginId, string password)
+        /// <summary>
+        /// 登录
+        /// </summary>
+        /// <param name="loginId">登录名</param>
+        /// <param name="password">密码</param>
+        /// <returns>登录信息</returns>
+        [HttpPost]
+        [WrapPostParameters]
+        public LoginInfo Login(string loginId, string password)
+        {
+            LoginInfo loginInfo = this._authenticationContract.Login(loginId, password);
+
+            return loginInfo;
+        }
+        #endregion
+    }
+}
---
+++ AppServices/SD.IdentitySystem.AppService.WebApi/Startup.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.Owin;
+using Owin;
+using SD.IdentitySystem.AppService.WebApi;
+using SD.IdentitySystem.WCFAuthentication.Owin;
+using SD.IOC.Integration.WebApi.SelfHost;
+using SD.Toolkits.Owin.Middlewares;
+using SD.Toolkits.WebApi.Extensions;
+using System.Web.Http;
+using System.Web.Http.Cors;
+
+[assembly: OwinStartup(typeof(Startup))]
+namespace SD.IdentitySystem.AppService.WebApi
+{
+    public class Startup : StartupBase
+    {
+        /// <summary>
+        /// 配置应用程序
+        /// </summary>
+        /// <param name="appBuilder">应用程序建造者</param>
+        /// <param name="httpConfiguration">Http配置</param>
+        protected override void Configuration(IAppBuilder appBuilder, HttpConfiguration httpConfiguration)
+        {
+            httpConfiguration.Routes.MapHttpRoute(
+                "DefaultApi",
+                "{controller}/{action}/{id}",
+                new { id = RouteParameter.Optional }
+            );
+
+            //POST请求多参数绑定
+            httpConfiguration.RegisterWrapParameterBindingRules();
+
+            //允许跨域
+            httpConfiguration.EnableCors(new EnableCorsAttribute("*", "*", "*"));
+
+            appBuilder.Use<CacheOwinContextMiddleware>();
+            appBuilder.Use<PublicKeyExchangeMiddleware>();
+        }
+    }
+}
