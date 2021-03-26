0dc3f46af9694c51d618271ad8681d75bc471356
Lee <lishilei0523@163.com>
2021-03-26 20:26:47 +0800

    临时提交；

---
+++ Extensions/SD.IdentitySystem.Membership.Core/Membership.cs
@@ -0,0 +1,31 @@
+﻿using SD.Infrastructure.MemberShip;
+
+// ReSharper disable once CheckNamespace
+namespace SD.IdentitySystem
+{
+    /// <summary>
+    /// Membership管理工具类
+    /// </summary>
+    public static class Membership
+    {
+        /// <summary>
+        /// 当前登录信息
+        /// </summary>
+        public static LoginInfo LoginInfo
+        {
+            get
+            {
+                //if (!context.HttpContext.Request.Headers.TryGetValue(SessionKey.CurrentPublicKey, out StringValues header))
+                //{
+                //    ObjectResult response = new ObjectResult("身份认证消息头不存在，请检查程序！")
+                //    {
+                //        StatusCode = (int)HttpStatusCode.Unauthorized
+                //    };
+                //    context.Result = response;
+                //}
+
+                return null;
+            }
+        }
+    }
+}
