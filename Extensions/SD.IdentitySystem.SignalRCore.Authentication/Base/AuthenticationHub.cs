dc788064527a3d1fbb94d59c056195f6cdb427e0
Lee <lishilei0523@163.com>
2021-04-02 13:39:47 +0800

    添加ASP.NET Core SignalR身份认证工具；

---
+++ Extensions/SD.IdentitySystem.SignalRCore.Authentication/Base/AuthenticationHub.cs
@@ -0,0 +1,95 @@
+﻿using SD.Infrastructure.MessageBase;
+using SD.Infrastructure.SignalRCore.Server.Base;
+using SD.Toolkits.AspNetCore;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SD.IdentitySystem.SignalRCore.Authentication.Base
+{
+    /// <summary>
+    /// 认证消息Hub基类
+    /// </summary>
+    public abstract class AuthenticationHub<T> : MessageHub<T> where T : IMessage
+    {
+        #region # 字段及构造器
+
+        /// <summary>
+        /// 同步锁
+        /// </summary>
+        private static readonly object _Sync;
+
+        /// <summary>
+        /// 用户/连接字典
+        /// </summary>
+        private static readonly IDictionary<string, string> _UserConnections;
+
+        /// <summary>
+        /// 静态构造器
+        /// </summary>
+        static AuthenticationHub()
+        {
+            _Sync = new object();
+            _UserConnections = new ConcurrentDictionary<string, string>();
+        }
+
+        #endregion
+
+        #region # 方法
+
+        #region 交换消息 —— override void Exchange(T message)
+        /// <summary>
+        /// 交换消息
+        /// </summary>
+        /// <param name="message">消息</param>
+        public override void Exchange(T message)
+        {
+            if (message.ReceiverAccounts != null && message.ReceiverAccounts.Any())
+            {
+                IList<string> receiverConnectionIds = _UserConnections.Where(x => message.ReceiverAccounts.Contains(x.Key)).Select(x => x.Value).ToList();
+
+                IMessageHub<T> messageHub = base.Clients.Clients(new ReadOnlyCollection<string>(receiverConnectionIds));
+                messageHub.Exchange(message);
+            }
+            else
+            {
+                base.Exchange(message);
+            }
+        }
+        #endregion
+
+        #region 客户端连接事件 —— override Task OnConnectedAsync()
+        /// <summary>
+        /// 客户端连接事件
+        /// </summary>
+        public override Task OnConnectedAsync()
+        {
+            if (AspNetCoreSection.Setting.Authorized)
+            {
+                lock (_Sync)
+                {
+                    string loginId = base.Context.User.Identity.Name;
+
+                    //用户/连接字典
+                    if (_UserConnections.ContainsKey(loginId))
+                    {
+                        _UserConnections[loginId] = base.Context.ConnectionId;
+                    }
+                    else
+                    {
+                        _UserConnections.Add(loginId, base.Context.ConnectionId);
+                    }
+
+                    return base.OnConnectedAsync();
+                }
+            }
+
+            return base.OnConnectedAsync();
+        }
+        #endregion
+
+        #endregion
+    }
+}
---
+++ Extensions/SD.IdentitySystem.SignalRCore.Authentication/Middlewares/AuthenticationMiddleware.cs
@@ -0,0 +1,74 @@
+﻿using Microsoft.AspNetCore.Http;
+using SD.CacheManager;
+using SD.Infrastructure.Constants;
+using SD.Infrastructure.MemberShip;
+using SD.Toolkits.AspNetCore;
+using System.Net;
+using System.Security.Principal;
+using System.Threading.Tasks;
+
+namespace SD.IdentitySystem.SignalRCore.Authentication.Middlewares
+{
+    /// <summary>
+    /// 身份认证中间件
+    /// </summary>
+    public class AuthenticationMiddleware
+    {
+        /// <summary>
+        /// 请求委托
+        /// </summary>
+        private readonly RequestDelegate _next;
+
+        /// <summary>
+        /// 依赖注入构造器
+        /// </summary>
+        public AuthenticationMiddleware(RequestDelegate next)
+        {
+            this._next = next;
+        }
+
+        /// <summary>
+        /// 执行中间件
+        /// </summary>
+        public async Task Invoke(HttpContext context)
+        {
+            if (AspNetCoreSection.Setting.Authorized)
+            {
+                //读Header
+                string publicKey = context.Request.Headers[SessionKey.CurrentPublicKey];
+                if (string.IsNullOrWhiteSpace(publicKey))
+                {
+                    //读QueryString
+                    publicKey = context.Request.Query[SessionKey.CurrentPublicKey];
+                }
+                if (string.IsNullOrWhiteSpace(publicKey))
+                {
+                    context.Response.StatusCode = (int)HttpStatusCode.Unauthorized;
+                    context.Response.Headers.Append("ErrorMessage", "Public key not found");
+
+                    await this._next.Invoke(context);
+                }
+                else
+                {
+                    LoginInfo loginInfo = CacheMediator.Get<LoginInfo>(publicKey);
+                    if (loginInfo == null)
+                    {
+                        context.Response.StatusCode = (int)HttpStatusCode.Unauthorized;
+                        context.Response.Headers.Append("ErrorMessage", "Login info expired");
+
+                        await this._next.Invoke(context);
+                    }
+                    else
+                    {
+                        IIdentity identity = new GenericIdentity(loginInfo.LoginId);
+                        context.User = new GenericPrincipal(identity, null);
+
+                        await this._next.Invoke(context);
+                    }
+                }
+            }
+
+            await this._next.Invoke(context);
+        }
+    }
+}
