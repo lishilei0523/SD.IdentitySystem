d07025091815bfaf1fb526f3eb491a413d05e1f2
Lee <lishilei0523@163.com>
2021-05-10 15:15:57 +0800

    菜单TreeListView实现；

--- Windows/SD.IdentitySystem.Client/ViewModels/MenuViewModel.cs
+++ Windows/SD.IdentitySystem.Client/ViewModels/MenuViewModel.cs
@@ -1,4 +1,12 @@
 ﻿using Caliburn.Micro;
+using SD.IdentitySystem.IPresentation.Interfaces;
+using SD.IdentitySystem.IPresentation.Models.Outputs;
+using SD.Infrastructure.WPF.Aspects;
+using SD.Infrastructure.WPF.Extensions;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Threading;
+using System.Threading.Tasks;
 
 namespace SD.IdentitySystem.Client.ViewModels
 {
@@ -7,6 +15,31 @@ namespace SD.IdentitySystem.Client.ViewModels
     /// </summary>
     public class MenuViewModel : Screen
     {
+        private readonly IMenuPresenter _menuPresenter;
 
+        /// <summary>Creates an instance of the screen.</summary>
+        public MenuViewModel(IMenuPresenter menuPresenter)
+        {
+            this._menuPresenter = menuPresenter;
+        }
+
+        [DependencyProperty]
+        public ObservableCollection<Menu> Menus { get; set; }
+
+
+
+        public async Task GetMenuTreeGrid()
+        {
+            LoadingIndicator.Suspend();
+
+            IEnumerable<Menu> menus = await Task.Run(() =>
+            {
+                Thread.Sleep(3000);
+                return this._menuPresenter.GetMenuTreeGrid(null, null);
+            });
+            LoadingIndicator.Dispose();
+
+            this.Menus = new ObservableCollection<Menu>(menus);
+        }
     }
 }
---
+++ Windows/SD.Infrastructure.WPF/Extensions/LoadingIndicator.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.ComponentModel;
+
+namespace SD.Infrastructure.WPF.Extensions
+{
+    /// <summary>
+    /// 加载指示器
+    /// </summary>
+    public static class LoadingIndicator
+    {
+        #region # 事件与属性
+
+        /// <summary>
+        /// 属性值变更事件
+        /// </summary>
+        public static event EventHandler<PropertyChangedEventArgs> IsBusyChanged;
+
+        /// <summary>
+        /// 是否繁忙
+        /// </summary>
+        [ThreadStatic]
+        private static bool _IsBusy;
+
+        /// <summary>
+        /// 是否繁忙
+        /// </summary>
+        public static bool IsBusy
+        {
+            get { return LoadingIndicator._IsBusy; }
+            set { LoadingIndicator._IsBusy = value; LoadingIndicator.IsBusyChanged?.Invoke(null, new PropertyChangedEventArgs(nameof(LoadingIndicator.IsBusy))); }
+        }
+
+        #endregion
+
+        #region # 方法
+
+        /// <summary>
+        /// 挂起加载指示器
+        /// </summary>
+        public static void Suspend()
+        {
+            LoadingIndicator.IsBusy = true;
+        }
+
+        /// <summary>
+        /// 释放加载指示器
+        /// </summary>
+        public static void Dispose()
+        {
+            LoadingIndicator.IsBusy = false;
+        }
+
+        #endregion
+    }
+}
