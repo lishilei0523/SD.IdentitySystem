029448ba38a9cda1ecf7da465af123cebe290ce0
Lee <lishilei0523@163.com>
2021-05-11 13:14:55 +0800

    行号bug修复；

--- Windows/SD.Infrastructure.WPF/Controls/TreeListView.cs
+++ Windows/SD.Infrastructure.WPF/Controls/TreeListView.cs
@@ -8,6 +8,11 @@ namespace SD.Infrastructure.WPF.Controls
     /// </summary>
     public class TreeListView : TreeView
     {
+        public TreeListView()
+        {
+            this.Loaded += (sender, _) => ((TreeListView)sender).Tag = null;
+        }
+
         protected override DependencyObject GetContainerForItemOverride()
         {
             return new TreeListViewItem();
