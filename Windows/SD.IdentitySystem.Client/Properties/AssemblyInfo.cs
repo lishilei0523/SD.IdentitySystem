9a851241cc6a6ad094a987e14786aa29fd8db64e
Lee <lishilei0523@163.com>
2021-06-24 20:44:57 +0800

    .csproj文件升级；

--- Windows/SD.IdentitySystem.Client/Properties/AssemblyInfo.cs
+++ Windows/SD.IdentitySystem.Client/Properties/AssemblyInfo.cs
@@ -6,12 +6,6 @@ using SD.Infrastructure.WPF.Caliburn.Aspects;
 // 有关程序集的一般信息由以下
 // 控制。更改这些特性值可修改
 // 与程序集关联的信息。
-[assembly: AssemblyTitle("SD.IdentitySystem.Client")]
-[assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("SD")]
-[assembly: AssemblyProduct("SD.IdentitySystem.Client")]
-[assembly: AssemblyCopyright("Copyright © SD 2021")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -40,18 +34,5 @@ using SD.Infrastructure.WPF.Caliburn.Aspects;
 )]
 
 
-// 程序集的版本信息由下列四个值组成: 
-//
-//      主版本
-//      次版本
-//      生成号
-//      修订号
-//
-//可以指定所有这些值，也可以使用“生成号”和“修订号”的默认值
-//通过使用 "*"，如下所示:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
-
-//异常AOP特性
+// 异常AOP特性
 [assembly: ExceptionAspect]
