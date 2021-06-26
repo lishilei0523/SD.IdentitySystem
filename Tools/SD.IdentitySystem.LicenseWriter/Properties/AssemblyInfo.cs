8333d39ab474ee4762eccac892a940d3090a29b4
Lee <lishilei0523@163.com>
2021-06-26 16:12:58 +0800

    .csproj文件升级；

--- Tools/SD.IdentitySystem.LicenseWriter/Properties/AssemblyInfo.cs
+++ Tools/SD.IdentitySystem.LicenseWriter/Properties/AssemblyInfo.cs
@@ -4,12 +4,6 @@ using System.Runtime.InteropServices;
 // 有关程序集的一般信息由以下
 // 控制。更改这些特性值可修改
 // 与程序集关联的信息。
-[assembly: AssemblyTitle("SD.IdentitySystem.LicenseWriter")]
-[assembly: AssemblyDescription("SD.IdentitySystem 许可证生成工具")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("SD")]
-[assembly: AssemblyProduct("SD.IdentitySystem.LicenseWriter")]
-[assembly: AssemblyCopyright("Copyright © SD 2021")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -20,16 +14,3 @@ using System.Runtime.InteropServices;
 
 // 如果此项目向 COM 公开，则下列 GUID 用于类型库的 ID
 [assembly: Guid("8b2c63c4-2f19-47a9-b45e-dabd4e379624")]
-
-// 程序集的版本信息由下列四个值组成: 
-//
-//      主版本
-//      次版本
-//      生成号
-//      修订号
-//
-// 可以指定所有值，也可以使用以下所示的 "*" 预置版本号和修订号
-// 方法是按如下所示使用“*”: :
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
