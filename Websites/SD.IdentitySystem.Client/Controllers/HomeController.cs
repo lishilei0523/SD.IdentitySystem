effca0f71521985dcf17d3aad0f9a6b8ca5bc1ea
Lee <lishilei0523@163.com>
2021-11-09 19:25:08 +0800

    修复bug；

--- Websites/SD.IdentitySystem.Client/Controllers/HomeController.cs
+++ Websites/SD.IdentitySystem.Client/Controllers/HomeController.cs
@@ -23,7 +23,22 @@ namespace SD.IdentitySystem.Client.Controllers
             base.ViewBag.LoginId = loginInfo?.LoginId;
             base.ViewBag.RealName = loginInfo?.RealName;
 
-            return this.View();
+            return base.View();
+        }
+        #endregion
+
+        #region # 加载异常视图 —— ViewResult Exception(string message)
+        /// <summary>
+        /// 加载异常视图
+        /// </summary>
+        /// <param name="message">异常消息</param>
+        /// <returns>异常视图</returns>
+        [RequireAuthorization("异常视图")]
+        public ViewResult Exception(string message)
+        {
+            base.ViewBag.ErrorMessage = message;
+
+            return base.View();
         }
         #endregion
     }
