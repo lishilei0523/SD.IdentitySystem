2bdf63b073cec1e8dec2174858fb9c6a1324243e
Lee <lishilei0523@163.com>
2021-11-16 20:32:33 +0800

    客户端自动更新服务增加ASP.NET Core版；

---
+++ Windows/SD.IdentitySystem.UpdateService(Core)/Properties/AssemblyInfo.cs
@@ -0,0 +1,12 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTrademark("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("2234219b-76b4-4b7f-99c3-661c6520ce6c")]
---
+++ Windows/SD.IdentitySystem.UpdateService(Core)/Startup.cs
@@ -0,0 +1,37 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.FileProviders;
+using SD.Toolkits.AspNet;
+using System;
+using System.IO;
+
+namespace SD.IdentitySystem.UpdateService
+{
+    /// <summary>
+    /// 应用程序启动器
+    /// </summary>
+    public class Startup
+    {
+        /// <summary>
+        /// 配置应用程序
+        /// </summary>
+        public void Configure(IApplicationBuilder appBuilder)
+        {
+            //配置服务器
+            string staticFilesRoot = Path.Combine(AppContext.BaseDirectory, AspNetSetting.StaticFilesPath);
+            string fileServerRoot = Path.Combine(AppContext.BaseDirectory, AspNetSetting.FileServerPath);
+            Directory.CreateDirectory(staticFilesRoot);
+            Directory.CreateDirectory(fileServerRoot);
+            StaticFileOptions staticFileOptions = new StaticFileOptions
+            {
+                FileProvider = new PhysicalFileProvider(staticFilesRoot)
+            };
+            FileServerOptions fileServerOptions = new FileServerOptions
+            {
+                FileProvider = new PhysicalFileProvider(fileServerRoot),
+                EnableDirectoryBrowsing = true
+            };
+            appBuilder.UseStaticFiles(staticFileOptions);
+            appBuilder.UseFileServer(fileServerOptions);
+        }
+    }
+}
